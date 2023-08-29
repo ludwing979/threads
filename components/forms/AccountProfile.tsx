121406d0feec96a3442f3ca7f754bd1ff8e78ee0
ludwing979 <magoludwing@gmail.com>
2023-08-29 14:07:14 -0600

    Adding functionality to manipulate images

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -15,9 +15,11 @@ import { Textarea } from "../ui/textarea"
 import { useForm } from "react-hook-form"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { UserValidation } from "@/lib/validations/user"
+import { ChangeEvent, useState } from "react"
+import { isBase64Image } from "@/lib/utils"
+import { useUploadThing } from "@/lib/uploadthing"
 import Image from "next/image"
 import * as z from "zod"
-import { ChangeEvent, useState } from "react"
 
 interface Props {
   user: {
@@ -33,6 +35,7 @@ interface Props {
 
 const AccountProfile = ({ user, btnTitle }: Props) => {
   const [files, setFiles] = useState<File[]>([])
+  const { startUpload } = useUploadThing("media")
 
   const form = useForm({
     resolver: zodResolver(UserValidation),
@@ -66,10 +69,18 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
     }
   }
 
-  function onSubmit(values: z.infer<typeof UserValidation>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
-    console.log(values)
+  const onSubmit = async (values: z.infer<typeof UserValidation>) => {
+    const blob = values.profile_photo
+
+    const hasImageChanged = isBase64Image(blob)
+
+    if (hasImageChanged) {
+      const imgRes = await startUpload(files)
+
+      if (imgRes && imgRes[0].fileUrl) {
+        values.profile_photo = imgRes[0].fileUrl
+      }
+    }
   }
 
   return (
